fb016371841f119f4cf5e39ae2dc7d748da45399
haUk2 <91018531+haUk2@users.noreply.github.com>
2022-10-21 15:07:52 +0200

    Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using Singleton;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+
+        King Heinrich = King.getInstance();
+
+        Heinrich.setName("Heinrich IV");
+
+        Console.WriteLine(Heinrich.Angriffsbefehl());
+
+        King Ludwig = King.getInstance();
+
+        Ludwig.setName("Ludwig XIV");
+
+        Console.WriteLine(Ludwig.Angriffsbefehl());
+
+        Console.WriteLine(Heinrich.Rueckzugsbefehl());
+
+    }
+}
